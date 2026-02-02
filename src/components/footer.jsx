a5dc463eade51dc4aadd3e7e3f98cdb8d6c77dce
Remy Johnny <ewerejohnnyjnr93@gmail.com>
2026-02-02 16:47:18 +0300

    changed url in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="rounded shadow-xs m-4">
         <div className="w-full max-w-7xl mx-auto p-4 md:py-8">
-        <span className="block text-sm text-gray-300 sm:text-center">© 2026 <a href="https://flowbite.com/" className="hover:underline">Remy™</a>. All Rights Reserved.</span>
+        <span className="block text-sm text-gray-300 sm:text-center">© 2026 <a href="https://www.instagram.com/remy_xle" className="hover:underline">Remy™</a>. All Rights Reserved.</span>
         </div>
     </footer>
   );

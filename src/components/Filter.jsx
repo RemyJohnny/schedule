f37d22a16c3a22ab49f857f6ac39afade3f08714
Remy Johnny <ewerejohnnyjnr93@gmail.com>
2026-09-04 03:03:17 +0300

    date picker range contraint implemented

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -10,7 +10,7 @@ export function Filter({date, subGroup }) {
             <Clock />
         </div>
         <div className="flex justify-between">
-            <Datepicker value={date.selectedDate} onChange={date.setSelectedDate} minDate={new Date(2026, 0, 23)} maxDate={new Date(2026, 4, 30)} language="RU" labelTodayButton="сегодня" labelClearButton="очистить"/>
+            <Datepicker value={date.selectedDate} onChange={date.setSelectedDate} minDate={new Date(2026, 8, 1)} maxDate={new Date(2027, 0, 1)} language="RU" labelTodayButton="сегодня" labelClearButton="очистить"/>
             <SubGroupDropdown subgroup={ subGroup }/>
         </div>
     </div>
